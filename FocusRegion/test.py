fc0b8435244632e43b297908e14233331430fbdc
kyle clarkson <clarkson@ubc.ca>
2020-09-08 20:40:47 -0700

    Added query region function.
    Outlined todo list.

--- FocusRegion/test.py
+++ FocusRegion/test.py
@@ -1,32 +1,57 @@
 import numpy as np
 import cv2
+# from skimage.measure import structural_similarity as ssim
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # capture consecutive frames.
-    ret1, frame1 = cap.read()
-    ret2, frame2 = cap.read()
+def mse(img1, img2):
+    """ Compute mean square error of two images. """
+    assert img1.shape == img2.shape
 
-    # Convert to gray scale, normalize to [0,1]
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    err = np.sum((img1.astype('float') - img2.astype("float")) ** 2 )
+    return err / float(img1.shape[0] * img1.shape[1])
 
-    img1 = np.zeros(gray1.shape)
-    img2 = np.zeros(gray2.shape)
 
-    img1 = cv2.normalize(gray1, img1, 0, 1, cv2.NORM_MINMAX)
-    img2 = cv2.normalize(gray2, img2, 0, 1, cv2.NORM_MINMAX)
+def query_region(img1, img2, x1, x2, y1, y2):
+    """ Replace subimage centered at center of img1 with
+     corresponding the subimage from img2. Return new image. """
+    result = img1.copy()
+    result[y1:y2, x1:x2] = img2[y1:y2, x1:x2]
+    return result
 
-    # TODO take difference, clip
-    diff = img1-img2
-    cv2.imshow('frame', diff)
-    # break
-    # Display the resulting frame
-    # cv2.imshow('frame', gray1)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    isCapturing = True
+    while isCapturing:
+        # capture consecutive frames.
+        ret1, frame1 = cap.read()
+        ret2, frame2 = cap.read()
+
+        frame_height = frame1.shape[0]
+        frame_width = frame1.shape[1]
+
+        # Convert to gray scale.
+        gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+
+        """
+        TODO
+        - Capture two consecutive images img1, img2. 
+        - Allow agent to choose k (hyper.param) centers of various sizes (model learns)
+            to update img1 with corresponding subimages from img2. Quality of choice is 
+            determined by an error (e.g. mse between img2 and updated img1.)
+        - Allow training on various videos.
+        - (?) Different way to represent image than raw image? 
+            Dimensional reduction? 
+        """
+        # Display the resulting frame
+        # cv2.imshow('frame', diff)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            isCapturing = False
+
+    # Release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
